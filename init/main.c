91df08fb842b6d50d78e88d8e6be5114436691b3
Fan Jiang <i@fanjiang.me>
2017-08-17 20:26:23 +0800

    rt100 PREEMPT-RT Patch

--- init/main.c
+++ init/main.c
@@ -76,6 +76,7 @@
 #include <linux/elevator.h>
 #include <linux/random.h>
 #include <linux/sched_clock.h>
+#include <linux/posix-timers.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -506,6 +507,7 @@ asmlinkage void __init start_kernel(void)
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
 	setup_per_cpu_areas();
+	softirq_early_init();
 	smp_prepare_boot_cpu();	/* arch-specific boot-cpu hooks */
 
 	build_all_zonelists(NULL, NULL);
